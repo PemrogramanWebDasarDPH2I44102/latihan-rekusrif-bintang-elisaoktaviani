1655b2fa6ec478afca8cfb0f38d3f3399cae1349
elisaoktaviani <42663031+elisaoktaviani@users.noreply.github.com>
2018-09-13 08:59:19 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,37 @@
 <?php
+$pola = 5;
+    function rekursif($h,$a){
+        if($h>0){
+            if($a>0){
+                echo "* ";
+                $a--;
+                rekursif($h,$a);
+            }else{
+                echo"<br>";
+                $h--;
+                rekursif($h,$h);           
+                 }
+            }
+    }
+    rekursif($pola,$pola);
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
-rekursif(5);
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "*/==========================================*/<br>";
+ $pola = 5;
+    function pattern($b,$n, $pola){
+        if($b<=$pola){
+            if($n<=$b){
+                echo "$n";
+                $n++;
+                pattern($b,$n,$pola);
+            }else{
+                echo"<br>";
+                $b++;
+                pattern($b,1,$pola);           
+                 }
+            }
+    }
+    pattern(1,1,$pola);
 ?>
